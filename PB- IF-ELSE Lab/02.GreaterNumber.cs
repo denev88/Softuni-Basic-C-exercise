5dd59afa82dc4938abc7ca769871d740d846fde5
denev88 <119948803+denev88@users.noreply.github.com>
2022-12-12 11:30:39 +0200

    Add task for  exercise lab
    Problem Solution for PB IF-ELSE lab

---
+++ PB- IF-ELSE Lab/02.GreaterNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Greater_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine()); 
+
+            if(numOne > numTwo)
+            {
+                Console.WriteLine(numOne);
+            }
+            else
+            {
+                Console.WriteLine(numTwo);
+            }
+        }
+    }
+}
