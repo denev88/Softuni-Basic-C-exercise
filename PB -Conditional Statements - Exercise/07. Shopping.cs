db7960a3f27c34a301e2c2f86fd9eb4533aed324
denev88 <119948803+denev88@users.noreply.github.com>
2022-12-16 23:32:04 +0200

    Add files via upload

---
+++ PB -Conditional Statements - Exercise/07. Shopping.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _07._Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Бюджетът на Петър -реално число в интервала[0.0…100000.0]
+            //
+            //2.Броят видеокарти - цяло число в интервала[0…100]
+            //
+            //3.Броят процесори - цяло число в интервала[0…100]
+            //
+            //4.Броят рам памет -цяло число в интервала[0…100]
+
+            double budgetPeter = double.Parse(Console.ReadLine());
+            int numberOfVideocards = int.Parse(Console.ReadLine());
+            int numberOfProcesors = int.Parse(Console.ReadLine());
+            int numberOfRam = int.Parse(Console.ReadLine());
+
+            //· Видеокарта – 250 лв./ бр.
+            //
+            //· Процесор – 35 % от цената на закупените видеокарти/ бр.
+            //
+            //· Рам памет – 10 % от цената на закупените видеокарти/ бр
+
+            int videocardPrice = 250;
+            double discountProcesors = 0.35;
+            double discountRam = 0.10;
+            double totalDiscount = 0.15;
+
+            int totalPriceVideocard = numberOfVideocards * videocardPrice;
+
+           double priceofProcesors = totalPriceVideocard * discountProcesors;
+            double totalProcesorsPrice = priceofProcesors * numberOfProcesors;
+
+           double priceOfRam = totalPriceVideocard * discountRam;
+            double totalRamPrice = priceOfRam * numberOfRam;
+
+            double totalSum = totalPriceVideocard + totalProcesorsPrice + totalRamPrice;
+           
+              double Sum = totalSum - (totalSum * totalDiscount);
+           
+
+
+            if ( budgetPeter  >= Sum)
+            {
+
+                double finalSum = budgetPeter - Sum;
+                Console.WriteLine($"You have {Math.Abs(finalSum):f2} leva left!");
+
+            }
+            else
+            {
+                double sumFinal = budgetPeter - Sum;
+
+                Console.WriteLine($"Not enough money! You need {Math.Abs(sumFinal):f2} leva more!");
+            }
+                 
+
+
+
+        }
+    }
+}
---
+++ PB -Conditional Statements - Exercise/08. Lunch Break.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _08._Lunch_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.Име на сериал – текст
+            //
+            //2.Продължителност на епизод – цяло число в диапазона[10… 90]
+            //
+            //3.Продължителност на почивката – цяло число в диапазона[10… 120]
+
+            string serialName = Console.ReadLine();
+            double episodeDuration = int.Parse(Console.ReadLine());
+            double breakDuration = int.Parse(Console.ReadLine());
+
+            double timeForLunch = breakDuration * 0.125;
+            double timeForBreak = breakDuration * 0.25;
+            double othersTime = breakDuration - timeForLunch - timeForBreak;
+            
+            if (othersTime >= episodeDuration)
+            {
+                //double timeOne = Math.Ceiling(othersTime - episodeDuration);
+
+
+                Console.WriteLine($"You have enough time to watch {serialName}" +
+                    $" and left with {Math.Ceiling(othersTime - episodeDuration)} minutes free time.");
+            }
+            else 
+            {
+                //double timeTwo = Math.Ceiling(episodeDuration - othersTime);
+
+                Console.WriteLine($"You don't have enough time to watch {serialName}, " +
+                    $"you need {Math.Ceiling(episodeDuration - othersTime )} more minutes.");
+            }
+
+        }
+    }
+}
